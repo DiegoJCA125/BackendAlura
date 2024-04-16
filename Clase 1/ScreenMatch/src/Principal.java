1bd9593071575394c6fdb14c87be8f88b0f3959e
Juan Diego Carmona Arredondo <104167947+DiegoJCA125@users.noreply.github.com>
2024-04-16 17:55:37 -0500

    Add files via upload

--- Clase 1/ScreenMatch/src/Principal.java
+++ Clase 1/ScreenMatch/src/Principal.java
@@ -1,37 +1,55 @@
+import com.aluracursos.screenmatch.modelos.Pelicula;
+
 public class Principal {
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula();
-        miPelicula.nombre = "Scarface";
-        miPelicula.fechaDeLanzamiento = 1983;
-        miPelicula.duracionEnMinutos = 130;
+        miPelicula.setNombre("Scarface");
+        miPelicula.setFechaDeLanzamiento(1983);
+        miPelicula.setDuracionEnMinutos(130);
+        miPelicula.setIncluidoEnElPlan(true);
+
+        miPelicula.evalua(10);
+        miPelicula.evalua(10);
 
         miPelicula.muestraFichaTecnica();
-        miPelicula.evalua(10);
-        miPelicula.evalua(10);
+        System.out.println("Total de votos: "+ miPelicula.getTotalDeLasEvaluaciones());
+        System.out.println("Media de evalucion de la pelicula: " + miPelicula.calculaMedia());
+        System.out.println("**********************");
 
-        System.out.println(miPelicula.sumaDelasEvaluaciones);
-        System.out.println(miPelicula.totalDeLasEvaluciones);
-        System.out.println(miPelicula.calculaMedia());
+
+
+
+
+
 
         Pelicula otraPelicula = new Pelicula();
-        otraPelicula.nombre = "Matrix";
-        otraPelicula.fechaDeLanzamiento = 1998;
-        otraPelicula.duracionEnMinutos = 180;
-        System.out.println("Média de evaluaciones de la película: " +miPelicula.calculaMedia());
+        otraPelicula.setNombre("Matrix");
+        otraPelicula.setFechaDeLanzamiento(1998);
+        otraPelicula.setDuracionEnMinutos(180);
+
+        otraPelicula.evalua(9);
+        otraPelicula.evalua(8);
+        otraPelicula.evalua(9.8);
 
         otraPelicula.muestraFichaTecnica();
+        System.out.println("Total de votos: " + otraPelicula.getTotalDeLasEvaluaciones());
+        System.out.println("Média de evaluaciones de la película: " +otraPelicula.calculaMedia());
+        System.out.println("**********************");
 
         Pelicula tercerPelicula = new Pelicula();
-        miPelicula.nombre = "La familia del futuro";
-        miPelicula.fechaDeLanzamiento = 2007;
-        miPelicula.duracionEnMinutos = 95;
-        miPelicula.incluidoEnElPlan = true;
+        tercerPelicula.setNombre("La familia del futuro");
+        tercerPelicula.setFechaDeLanzamiento(2007);
+        tercerPelicula.setDuracionEnMinutos(95);
 
-        miPelicula.muestraFichaTecnica();
-        miPelicula.evalua(10);
-        miPelicula.evalua(10);
-        miPelicula.evalua(7.8);
-        System.out.println("Média de evaluaciones de la película: " +miPelicula.calculaMedia());
+        tercerPelicula.evalua(10);
+        tercerPelicula.evalua(10);
+        tercerPelicula.evalua(7.8);
+        tercerPelicula.evalua(6.8);
+
+        tercerPelicula.muestraFichaTecnica();
+        System.out.println("Total de votos: " + tercerPelicula.getTotalDeLasEvaluaciones());
+        System.out.println("Média de evaluaciones de la película: " + tercerPelicula.calculaMedia());
+        System.out.println("**********************");
     }
 }
 
